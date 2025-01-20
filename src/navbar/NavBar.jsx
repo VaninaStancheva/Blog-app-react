a51b0ba5c04dc4ddf99ba67f3f3be564df483997
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-01-20 13:59:56 +0200

    navBar - beginning

---
+++ src/navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+import './navbar.css'
+
+export default function NavBar (){
+    return (
+        <div className="navbar">
+            <div className="navLeft">left</div>
+            <div className="navCenter">center</div>
+            <div className="navRight">right</div>
+        </div>
+    )
+}
