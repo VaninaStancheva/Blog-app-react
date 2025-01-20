368f8c2437d15808aba24df8782ceaf5bc978b9b
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-01-20 16:54:50 +0200

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="headerTitles">
+                <span className="headerTitleSm">The thoughts of a </span>
+                <span className="headerTitleLg">programmer...</span>
+            </div>
+        </div>
+    );
+}
