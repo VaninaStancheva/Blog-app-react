832d731c44b363c2a3734daccc7181b272ce0ffc
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-01-20 15:20:53 +0200

    styling navbar

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -3,9 +3,24 @@ import './navbar.css'
 export default function NavBar (){
     return (
         <div className="navbar">
-            <div className="navLeft">left</div>
-            <div className="navCenter">center</div>
-            <div className="navRight">right</div>
+            <div className="navLeft">
+                <i className="navIcon fa-brands fa-square-facebook"></i>
+                <i className="navIcon fa-brands fa-square-x-twitter"></i>
+                <i className="navIcon fa-brands fa-linkedin"></i></div>
+            <div className="navCenter">
+                <ul className="navList">
+                    <li className="navListItem">HOME</li>
+                    <li className="navListItem">ABOUT</li>
+                    <li className="navListItem">CONTACT</li>
+                    <li className="navListItem">WRITE</li>
+                    <li className="navListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="navRight">
+                <img className='navImg' src="../../public/envato-labs-ai-93cd669d-a337-49b7-bae2-16597d5cc558.jpg"
+                     alt="profile-img"/>
+                <i className="searchIcon fa-solid fa-magnifying-glass"></i>
+            </div>
         </div>
     )
 }
