763db2fb092e6cfae6e80fdae0a530c8d8282b6b
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-01-21 07:28:25 +0200

    some style

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,9 +4,9 @@ export default function NavBar (){
     return (
         <div className="navbar">
             <div className="navLeft">
-                <i className="navIcon fa-brands fa-square-facebook"></i>
-                <i className="navIcon fa-brands fa-square-x-twitter"></i>
-                <i className="navIcon fa-brands fa-linkedin"></i></div>
+                <i className="searchIcon fa-solid fa-magnifying-glass"></i>
+                <input className="input-search" type="text" placeholder="Search" />
+            </div>
             <div className="navCenter">
                 <ul className="navList">
                     <li className="navListItem">HOME</li>
@@ -19,7 +19,6 @@ export default function NavBar (){
             <div className="navRight">
                 <img className='navImg' src="../../../public/envato-labs-ai-93cd669d-a337-49b7-bae2-16597d5cc558.jpg"
                      alt="profile-img"/>
-                <i className="searchIcon fa-solid fa-magnifying-glass"></i>
             </div>
         </div>
     )
