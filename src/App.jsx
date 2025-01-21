c22ed4f78f96b470c850d1e0905681b78aacdd9d
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-01-21 16:46:09 +0200

    single post page and single post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import NavBar from './components/navbar/NavBar.jsx';
 import Homepage from "./pages/homePage/Homepage.jsx";
+import SinglePost from "./pages/singlePostPage/Single.jsx";
+import SinglePostPage from "./pages/singlePostPage/Single.jsx";
 function App() {
 
   return (
     <>
      <div className="App">
          <NavBar />
-         <Homepage />
+         <SinglePostPage />
      </div>
     </>
   )
---
+++ src/pages/singlePostPage/Single.jsx
@@ -0,0 +1,11 @@
+import './single.css'
+import SinglePost from "../../components/singlePost/SinglePost.jsx";
+
+export default function SinglePostPage() {
+    return (
+        <div className="single">
+            <SinglePost />
+        </div>
+    )
+
+}
