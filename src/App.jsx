7ea6648300dd868e092377e186be738df4441520
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-01-21 17:40:54 +0200

    adding some routs and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import NavBar from './components/navbar/NavBar.jsx';
 import Homepage from "./pages/homePage/Homepage.jsx";
-import SinglePost from "./pages/singlePostPage/Single.jsx";
 import SinglePostPage from "./pages/singlePostPage/Single.jsx";
+import Posts from "./components/posts/Posts.jsx";
+
 function App() {
 
   return (
-    <>
-     <div className="App">
-         <NavBar />
-         <SinglePostPage />
-     </div>
-    </>
+          <Router>
+              <NavBar />
+              <Routes>
+                  <Route exact path="/" element={<Homepage />} />
+              </Routes>
+              <Routes>
+                  <Route path="/post/:postId" element={<SinglePostPage />} />
+              </Routes>
+          </Router>
   )
 }
 
